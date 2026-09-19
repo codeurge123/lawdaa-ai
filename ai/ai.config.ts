3434b10e652406a5c8b3f79d61a4e4cd8c0a5ccc
codeurge123 <bansalyash316@gmail.com>
2026-09-19 19:46:32 +0530

    some build fixes

--- ai/ai.config.ts
+++ ai/ai.config.ts
@@ -7,18 +7,16 @@ dotenv.config({
 
 
 export function getAgentModel() {
-
-
     const provider = createOpenRouter({
         apiKey: process.env.OPENROUTER_API_KEY,
     });
 
-    // now we also want the modelID
     const modelID = process.env.OPENROUTER_DEFAULT_MODE;
 
+    if (!modelID) {
+        throw new Error("OPENROUTER_DEFAULT_MODE is not defined in .env");
+    }
 
     return provider(modelID);
-
 }
-
 // ye wala part responsible hai LLM se communication ke leya
